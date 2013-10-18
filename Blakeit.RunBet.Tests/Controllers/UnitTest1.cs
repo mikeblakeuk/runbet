a6b5a2439a2e4d6ae325cad45042d586aa165277
mikeblakeuk <mikeblake@hotmail.com>
2013-10-18 16:47:45 +0000

    V2

---
+++ Blakeit.RunBet.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Blakeit.RunBet.Controllers;
+
+namespace Blakeit.RunBet.Tests.Controllers
+{
+    [TestClass]
+    public class RunnersControllerTests
+    {
+        [TestMethod]
+        public void CanGetAListOfRunners()
+        {
+            var runners = new RunnersController().GetRunners();
+            Assert.AreEqual(1, runners.Count);
+        }
+
+        [TestMethod]
+        public void CanPlaceABet()
+        {
+            var id = 1;// "mblake";
+            var span = new TimeSpan(1, 35, 45);
+            var vm = new RunnersController().MakeBet(id, span);
+            
+        }
+    }
+}
